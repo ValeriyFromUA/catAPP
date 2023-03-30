9ed249a6b7d8000ce218c943d4a9417a6d4dcca1
Vasylenko Valeriy <hitehnik132@gmail.com>
2023-03-30 07:48:36 +0300

    added likes

--- my_app/catapp/views.py
+++ my_app/catapp/views.py
@@ -140,6 +140,26 @@ def post_details(request, pk):
     post = Posts.objects.get(id=pk)
     images = Images.objects.filter(post_id=pk).all()
     tags = PostTags.objects.filter(post_id=pk).all()
-    context = {'post': post, 'images': images, 'tags': tags}
+
+    if request.method == 'POST':
+        user = request.user
+        is_liked = bool(request.POST.get('is_liked'))
+
+        like = Likes.objects.filter(user_id=user, post_id=post).first()
+        if like:
+
+            if like.is_liked == is_liked:
+                like.delete()
+            else:
+                like.is_liked = is_liked
+                like.save()
+        else:
+            Likes.objects.create(user_id=user, post_id=post, is_liked=is_liked)
+        return redirect('post', pk=pk)
+    likes_count = Likes.objects.filter(post_id=post, is_liked=True).count()
+    dislikes_count = Likes.objects.filter(post_id=post, is_liked=False).count()
+
+    context = {'post': post, 'images': images, 'tags': tags, 'likes_count': likes_count,
+               'dislikes_count': dislikes_count}
 
     return render(request, 'post.html', context)
